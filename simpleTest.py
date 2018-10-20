7cdbb0440ec9bc8c4d76f9b146371cc574be7d56
lmotnikar <lenart.motnikar@gmail.com>
2018-10-20 15:30:28 +0200

    Merge branch 'master' of C:\Users\lenar\PycharmProjects\project_robotic_farkas with conflicts.

--- simpleTest.py
+++ simpleTest.py
@@ -23,8 +23,8 @@ except:
 import time
 
 print ('Program started')
-vrep.simxFinish(-1) # just in case, close all opened connections
-clientID=vrep.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to V-REP
+vrep.simxFinish(-1)  # just in case, close all opened connections
+clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)  # Connect to V-REP
 if clientID!=-1:
     print ('Connected to remote API server')
 
