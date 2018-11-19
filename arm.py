68f9d3853ae439221586276cacb159c96cf9779c
lmotnikar <lenart.motnikar@gmail.com>
2018-11-19 20:19:37 +0100

    Implemented collision and updated main_scene accordingly. Still not working perfectly.

--- arm.py
+++ arm.py
@@ -36,6 +36,44 @@ class ArmController:
         return vrep.simxGetObjectFloatParameter(self.clientID, self.armHandle, vrep.sim_objfloatparam_modelbbox_min_z,
                                                 vrep.simx_opmode_blocking)[1] > -0.0423
 
+    def no_collision(self):
+        # Sometimes works, sometimes doesnt.... something with bounding boxes... I don't know, test before using.
+
+        link1_handle = vrep.simxGetObjectHandle(self.clientID, 'PhantomXPincher_link1', vrep.simx_opmode_oneshot_wait)[1]
+        min_x1 = vrep.simxGetObjectFloatParameter(self.clientID, link1_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_min_x, vrep.simx_opmode_blocking)[1]
+        max_x1 = vrep.simxGetObjectFloatParameter(self.clientID, link1_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_max_x, vrep.simx_opmode_blocking)[1]
+        min_y1 = vrep.simxGetObjectFloatParameter(self.clientID, link1_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_min_y, vrep.simx_opmode_blocking)[1]
+        max_y1 = vrep.simxGetObjectFloatParameter(self.clientID, link1_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_max_y, vrep.simx_opmode_blocking)[1]
+        min_z1 = vrep.simxGetObjectFloatParameter(self.clientID, link1_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_min_z, vrep.simx_opmode_blocking)[1]
+        max_z1 = vrep.simxGetObjectFloatParameter(self.clientID, link1_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_max_z, vrep.simx_opmode_blocking)[1]
+        x1, y1, z1 = vrep.simxGetObjectPosition(self.clientID, self.tip, -1, vrep.simx_opmode_blocking)[1]
+
+        link2_handle = vrep.simxGetObjectHandle(self.clientID, 'PhantomXPincher_link2', vrep.simx_opmode_oneshot_wait)[1]
+        min_x2 = vrep.simxGetObjectFloatParameter(self.clientID, link2_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_min_x, vrep.simx_opmode_blocking)[1]
+        max_x2 = vrep.simxGetObjectFloatParameter(self.clientID, link2_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_max_x, vrep.simx_opmode_blocking)[1]
+        min_y2 = vrep.simxGetObjectFloatParameter(self.clientID, link2_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_min_y, vrep.simx_opmode_blocking)[1]
+        max_y2 = vrep.simxGetObjectFloatParameter(self.clientID, link2_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_max_y, vrep.simx_opmode_blocking)[1]
+        min_z2 = vrep.simxGetObjectFloatParameter(self.clientID, link2_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_min_z, vrep.simx_opmode_blocking)[1]
+        max_z2 = vrep.simxGetObjectFloatParameter(self.clientID, link2_handle,
+                                                  vrep.sim_objfloatparam_modelbbox_max_z, vrep.simx_opmode_blocking)[1]
+        x2, y2, z2 = vrep.simxGetObjectPosition(self.clientID, self.tip, -1, vrep.simx_opmode_blocking)[1]
+
+
+        return False if min_x1 < x1 < max_x1 and min_y1 < y1 < max_y1 and min_z1 < z1 < max_z1 \
+            or min_x2 < x2 < max_x2 and min_y2 < y2 < max_y2 and min_z2 < z2 < max_z2 else True
+
+
     def reset_object_position(self):
         # x, y, z = 0, 0.25, 0.0250                   # Default position
         """
@@ -55,11 +93,10 @@ class ArmController:
         for n in range(n_epochs):
 
             if log is not None:
-                log.log("********************************************* EPOCH:" + str(n) + "*********************************************")
+                log.log("*" * 45 + " EPOCH: " + str(n) + ' ' + "*" * 45)
 
             state_vect = [0.0] * 5
             _, object_vect = vrep.simxGetObjectPosition(self.clientID, self.objectHandle, -1, vrep.simx_opmode_blocking)
-            # if it makes more sense for you, you can also move this function to main.py
             ef = exploration_factor * (1 - n / n_epochs)
             val = model.fit_iter(np.array([object_vect + state_vect]), ef, max_iter, learning_decay, log)
 
